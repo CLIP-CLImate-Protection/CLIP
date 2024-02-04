50db028727622a2cc5e02aaf7cd2edf35e9be35d
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-02-04 15:04:54 +0900

    feat:add page

--- frontend/lib/pages/quest/view/component/quest_component.dart
+++ frontend/lib/pages/quest/view/component/quest_component.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:frontend/fastAPI/models/Quest.dart';
 import 'package:frontend/pages/quest/controller/quest_controller.dart';
+import 'package:frontend/pages/quest/view/quest_each_view_page.dart';
+import 'package:get/get.dart';
+
+import '../../../../common/common.dart';
 
 class QuestComponent extends StatelessWidget {
-  const QuestComponent({super.key});
+  final Quest model;
 
-  //final Quest model;
-
-  //const QuestComponent({Key? key, required this.model}) : super(key: key);
+  const QuestComponent({Key? key, required this.model}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +20,7 @@ class QuestComponent extends StatelessWidget {
     return InkWell(
         onTap: () async {
           //await controller.getQuest();
-          //Get.toNamed(QuestEachPage.url, arguments: model);
+          Get.toNamed(QuestEachPage.url, arguments: model);
         },
         child: SizedBox(
           width: 341,
@@ -48,13 +52,11 @@ class QuestComponent extends StatelessWidget {
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        const SizedBox(
+                        SizedBox(
                           width: 231,
-                          //height: 42,
                           child: Text(
-                            '주문시 일회용 수저/포크 받지 않고 주문하기',
-                            //'${model.questName}',
-                            style: TextStyle(
+                            '${model.questName}',
+                            style: const TextStyle(
                               color: Colors.black,
                               fontSize: 14,
                               fontFamily: 'Istok Web',
@@ -64,21 +66,20 @@ class QuestComponent extends StatelessWidget {
                           ),
                         ),
                         const Spacer(),
-                        //model.type=='event'?
-                        SvgPicture.asset('assets/icons/event_apple.svg'),
-                        // : Icon(
-                        //   FontAwesomeIcons.coins,
-                        //   color: Color(Common.coinColor),
-                        //   size: 22,
-                        // ),
+                        model.type == 'event'
+                            ? SvgPicture.asset('assets/icons/event_apple.svg')
+                            : Icon(
+                                FontAwesomeIcons.coins,
+                                color: Color(Common.coinColor),
+                                size: 22,
+                              ),
                         const SizedBox(
                           width: 10,
                         ),
-                        const Text(
-                          '10',
-                          //'${model.reward}',
+                        Text(
+                          '${model.reward}',
                           textAlign: TextAlign.center,
-                          style: TextStyle(
+                          style: const TextStyle(
                             color: Colors.black,
                             fontSize: 14,
                             fontFamily: 'Inter',
---
+++ frontend/lib/pages/quest/view/quest_each_view_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestEachPage extends StatelessWidget {
+  const QuestEachPage({super.key});
+
+  static const String url = '/quest/each';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

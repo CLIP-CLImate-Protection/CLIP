324b396a204dc2634c3f501938c08e42243cf63a
9-coding <9.coding.lab@gmail.com>
2024-02-07 11:13:20 +0900

    FEAT: add ImageUploader

--- lib/pages/questdetails/controller/questdetails_controller.dart
+++ lib/pages/questdetails/controller/questdetails_controller.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ExImageViewer extends StatefulWidget {
   const ExImageViewer({Key? key}) : super(key: key);
@@ -44,5 +46,55 @@ class _ExImageViewerState extends State<ExImageViewer> {
         ]
     );
   }
+}
 
+
+class ImageUploader extends StatefulWidget {
+  const ImageUploader({Key? key}) : super(key: key);
+
+  @override
+  State<ImageUploader> createState() => _ImageUploaderState();
+}
+
+class _ImageUploaderState extends State<ImageUploader> {
+  XFile? _image;
+  final ImagePicker picker = ImagePicker();
+
+  //이미지를 가져오는 함수
+  Future getImage(ImageSource imageSource) async {
+    final XFile? pickedFile = await picker.pickImage(source: imageSource);
+    if (pickedFile != null) {
+      setState(() {
+        _image = pickedFile;
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 242,
+        height: 199,
+        decoration: BoxDecoration(
+          color: const Color(0xFFF1F5F1),
+          borderRadius: BorderRadius.circular(15),
+      ),
+      child: InkWell(
+        child: Center(
+          child: _image == null
+              ? const Icon(
+            Icons.add,
+            size: 40,
+            color: Color(0xFF888888),
+          )
+              : Image.file(
+            File(_image!.path),
+            //fit: BoxFit.cover,
+          ),
+        ),
+        onTap: () async{
+          getImage(ImageSource.gallery);
+        }
+      )
+    );
+  }
 }
--- lib/pages/questdetails/view/imagequest.dart
+++ lib/pages/questdetails/view/imagequest.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import '../controller/questdetails_controller.dart';

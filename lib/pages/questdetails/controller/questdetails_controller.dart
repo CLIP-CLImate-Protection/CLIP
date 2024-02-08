d73e0bd42a6d586772ec1dbd35e736ba67c8c019
9-coding <9.coding.lab@gmail.com>
2024-02-08 23:42:10 +0900

    FEAT: add assets/model/labels.txt

--- lib/pages/questdetails/controller/questdetails_controller.dart
+++ lib/pages/questdetails/controller/questdetails_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 class ExImageViewer extends StatefulWidget {
   const ExImageViewer({Key? key}) : super(key: key);

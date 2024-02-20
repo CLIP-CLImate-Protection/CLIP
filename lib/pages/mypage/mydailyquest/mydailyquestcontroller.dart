3ea1b78784150e63da6743337159ed42f85357e9
2sojeong <dlgur200123@naver.com>
2024-02-20 20:01:10 +0900

    feat: update myquestlist

--- lib/pages/mypage/mydailyquest/mydailyquestcontroller.dart
+++ lib/pages/mypage/mydailyquest/mydailyquestcontroller.dart
@@ -1,5 +1,19 @@
+import 'package:frontend/backend/UserService/user_service.dart';
+import 'package:frontend/service/user_service.dart';
+
 class MyDailyQuestController {
-  Future<List<String>> getUserCompletedQuests() async {
-    return ['퀘스트 1', '퀘스트 2', '퀘스트 3']; // 더미 데이터
+  // Future<List<String>> getUserCompletedQuests() async {
+  //   return ['퀘스트 1', '퀘스트 2', '퀘스트 3']; // 더미 데이터
+  // }
+  Future<List<String>> getQuestDateList() async {
+    List<Map<String, dynamic>> jsonData =
+        await getUserQuestData(UserService.instance.uid);
+    List<String> quests = [];
+    for (var item in jsonData) {
+      for (var date in item.values) {
+        quests.add(date);
+      }
+    }
+    return quests;
   }
 }
